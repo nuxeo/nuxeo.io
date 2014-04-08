b8eef99a4cc09f3f13ec516bf897d88f9ade7cf6
Damien Metzler <dmetzler@gmail.com>
2014-04-08 16:04:35 +0200

    NXIO-142 use location format for nsproxy

--- fleet/io-container/start-post.sh
+++ fleet/io-container/start-post.sh
@@ -4,6 +4,9 @@ while ! docker ps | grep -q $ENV_TECH_ID
 do
     sleep 2
 done
+IP=`/opt/data/tools/resolve-ip.sh`
+PORT=`docker port $ENV_TECH_ID 8080 | awk -F : '{print $2}'`
 
-/usr/bin/etcdctl set /envs/$ENV_TECH_ID/port `docker port $ENV_TECH_ID 8080 | awk -F : '{print $2}'`
-/usr/bin/etcdctl set /envs/$ENV_TECH_ID/ip `/opt/data/tools/resolve-ip.sh`
+/usr/bin/etcdctl set /envs/$ENV_TECH_ID/port $PORT
+/usr/bin/etcdctl set /envs/$ENV_TECH_ID/ip $IP
+/usr/bin/etcdctl set /envs/$ENV_TECH_ID/location \{\"host\":\"$IP\",\"port\":$PORT\}
