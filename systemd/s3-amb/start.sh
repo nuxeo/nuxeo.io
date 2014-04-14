d213e1c7a07ba51e7d84b3eb771db71da7aa2024
Arnaud Kervern <akervern@nuxeo.com>
2014-04-14 15:47:42 +0200

    NXIO-143: Add missing file

---
+++ systemd/s3-amb/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Write service entries
+/usr/bin/etcdctl set /services/s3/1 \{\"host\":\"s3.amazonaws.com\",\"port\":80\}
+
+# Run Amazon S3 Ambassador container
+/usr/bin/docker run --rm --name s3-amb -P nuxeo/service-amb s3
