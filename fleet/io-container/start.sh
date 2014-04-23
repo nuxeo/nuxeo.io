25644659327e34ce048d0541df84da3ac191401f
Thomas Roger <throger@gmail.com>
2014-04-23 15:34:05 +0200

    NXIO-142: Generate postgres pwd using hex encode ouput

--- fleet/io-container/start.sh
+++ fleet/io-container/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 POSTGRES_AMB=postgres-amb
-PG_PWD=`openssl rand -base64 20`
+PG_PWD=`openssl rand -hex 15`
 /usr/bin/docker run --rm -P -t --name ${ENV_TECH_ID} --link ${POSTGRES_AMB}:db -e PG_DB_NAME=${ENV_TECH_ID} -e PG_ROLE_NAME=${ENV_TECH_ID} -e PG_PWD=${PG_PWD} -e ENV_TECH_ID=${ENV_TECH_ID} -e PGPASSWORD=nuxeoiopostgres ${DOCKER_REGISTRY}/nuxeo/iocontainer
