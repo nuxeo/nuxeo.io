0ac09983ffef799a3c3180082fe5045a78a199b2
Thomas Roger <throger@gmail.com>
2014-04-24 14:24:43 +0200

    NXIO-143: fix typos

--- fleet/vblob-service/start-post.sh
+++ fleet/vblob-service/start-post.sh
@@ -7,7 +7,7 @@ PORT=`docker port $VBLOB_NAME 9981 | awk -F : '{print $2}'`
 /opt/data/tools/register-service.sh $S3_SERVICE $PORT
 
 # Create the bucket on vBlob
-/opt/data/tools/create-vblob-bucket $BUCKET_NAME
+/opt/data/tools/create-vblob-bucket.sh $BUCKET_NAME
 
 # Write config entries
 /usr/bin/etcdctl set /config/s3/bucket $BUCKET_NAME
