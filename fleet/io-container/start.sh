4a2f168c75f7c6b4b5acb559d1e870dcb35bc605
Thomas Roger <throger@gmail.com>
2014-04-25 14:09:05 +0200

    NXIO-143: typo

--- fleet/io-container/start.sh
+++ fleet/io-container/start.sh
@@ -14,5 +14,5 @@ S3_REGION=`/usr/bin/etcdctl get /config/s3/region`
   -e PG_DB_NAME=${ENV_TECH_ID} -e PG_ROLE_NAME=${ENV_TECH_ID} -e PG_PWD=${PG_PWD} \
   -e PGPASSWORD=nuxeoiopostgres \
   -e S3_BUCKET=${S3_BUCKET} -e S3_AWSID=${S3_AWSID} -e S3_AWSSECRET=${S3_AWSSECRET} -e S3_REGION=${S3_REGION} \
-  -e ENV_TECH_ID=${ENV_TECH_ID}
+  -e ENV_TECH_ID=${ENV_TECH_ID} \
   ${DOCKER_REGISTRY}/nuxeo/iocontainer
