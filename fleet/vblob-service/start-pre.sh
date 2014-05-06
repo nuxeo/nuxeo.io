14c047f08d7876fd5a37ecb2f6f982ed9963da30
Thomas Roger <throger@gmail.com>
2014-05-06 10:49:27 +0200

    NXIO-143: build image only if not already built

--- fleet/vblob-service/start-pre.sh
+++ fleet/vblob-service/start-pre.sh
@@ -2,8 +2,12 @@
 
 REGISTRY=`/usr/bin/etcdctl get /docker/registry`
 
-# Build postgres image
-/usr/bin/docker build -t nuxeo/vblob /opt/data/docker/vblob/
+docker images | grep  "^nuxeo/vblob"
+if [ ! $? -eq 0 ]; then
+  echo "building image..."
+  # Build vblob image
+  /usr/bin/docker build -t nuxeo/vblob /opt/data/docker/vblob/
+fi
 
 # Tag and push container on registry
 /usr/bin/docker tag nuxeo/vblob $REGISTRY/nuxeo/vblob
