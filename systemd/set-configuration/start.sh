6611b7fb0ed9c1fb4d4ac24ed8fa5174d6567cc6
Thomas Roger <throger@gmail.com>
2014-05-23 10:51:20 +0200

    NXIO-172: remove set-configuration

--- systemd/set-configuration/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Write config for manager in dev
-/usr/bin/etcdctl set /services/manager/domain manager.trial.nuxeo.io.dev
-/usr/bin/etcdctl set /services/manager/defaultDnsSuffix trial.nuxeo.io.dev
