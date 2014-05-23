9fbe0cb6ee560acbee67720e62cbd6f0fc6c64d1
Thomas Roger <throger@gmail.com>
2014-05-23 17:41:11 +0200

    NXIO-172: start postgres and vblob only on Vagrant

--- systemd/init-cluster/start.sh
+++ systemd/init-cluster/start.sh
@@ -33,11 +33,15 @@ fi
 fleetctl start /opt/data/fleet/docker-registry/docker-registry.service
 /opt/data/tools/wait-fleet-unit.sh docker-registry.service
 
-fleetctl start /opt/data/fleet/postgres-service/postgres-service.service
-/opt/data/tools/wait-fleet-unit.sh postgres-service.service
+# start postgres and vblob only on Vagrant
+VIRT=`systemd-detect-virt`
+if [ $VIRT = "oracle" ] ; then
+  fleetctl start /opt/data/fleet/postgres-service/postgres-service.service
+  /opt/data/tools/wait-fleet-unit.sh postgres-service.service
 
-fleetctl start /opt/data/fleet/vblob-service/vblob-service.service
-/opt/data/tools/wait-fleet-unit.sh vblob-service.service
+  fleetctl start /opt/data/fleet/vblob-service/vblob-service.service
+  /opt/data/tools/wait-fleet-unit.sh vblob-service.service
+fi
 
 fleetctl start /opt/data/fleet/build-io-base/build-io-base.service
 /opt/data/tools/wait-fleet-unit.sh build-io-base.service
