44526b072fd7d6919dded04d3b92dd764eed9e9d
Arnaud Kervern <akervern@nuxeo.com>
2014-05-30 16:25:00 +0200

    Follow etcd-netfw repository changes

--- docker/service-amb/service-amb.sh
+++ docker/service-amb/service-amb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SERV=$1
-/usr/local/go/src/github.com/nuxeo/etcd-netfw/etcd-netfw -servicePath /services/${SERV} -etcdAddress http://172.17.42.1:4001
+/usr/local/go/src/github.com/arkenio/etcd-netfw/etcd-netfw -servicePath /services/${SERV} -etcdAddress http://172.17.42.1:4001
