3d78827630c1c62fd76cb3fa019d4e88352b13ca
Arnaud Kervern <akervern@nuxeo.com>
2014-06-09 15:23:58 +0200

    NXIO-188: Ensure to rm running local container when restart service

--- systemd/gogeta/start.sh
+++ systemd/gogeta/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/opt/data/tools/docker-clean.sh ${GOGETA_NAME} &> /dev/null
+
 /usr/bin/docker pull arken/gogeta
 /usr/bin/docker run --rm --name $GOGETA_NAME -p 7777:7777 arken/gogeta
 echo "Container $GOGETA_NAME is running."
--- systemd/s3-amb/start.sh
+++ systemd/s3-amb/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/opt/data/tools/docker-clean.sh ${S3_AMB_NAME} &> /dev/null
+
 # Run Amazon S3 Ambassador container
 /usr/bin/docker pull arken/service-amb
 /usr/bin/docker run --rm --name $S3_AMB_NAME -P arken/service-amb -servicePath /services/s3
