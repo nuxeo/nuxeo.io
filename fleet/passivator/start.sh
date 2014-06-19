5bd19b0075d4a2686efb8d70bcd2f3c1937a1d27
Arnaud Kervern <akervern@nuxeo.com>
2014-06-19 08:35:35 +0200

    NXIO-158: Ensure that passivator is correctly removed

--- fleet/passivator/start.sh
+++ fleet/passivator/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/opt/data/tools/docker-clean.sh ${PASSIVATOR_NAME} &> /dev/null
+
 /usr/bin/docker pull arken/passivator
 /usr/bin/docker run --rm --name $PASSIVATOR_NAME arken/passivator
 echo "Container $PASSIVATOR_NAME is running."
