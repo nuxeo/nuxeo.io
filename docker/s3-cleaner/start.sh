ad7ebe168e3300e1c3c6b75da46593e84e23b4bb
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-06-20 16:36:30 +0200

    NXIO-202: delete S3 bucket prefix content through REST API

--- docker/s3-cleaner/start.sh
+++ docker/s3-cleaner/start.sh
@@ -4,5 +4,7 @@
 sed -i 's/awsid/$S3_AWSID/g' /usr/bin/.s3curl
 sed -i 's/awssecret/$S3_AWSSECRET/g' /usr/bin/.s3curl
 
-# Delete S3 bucket
-/s3curl.pl --id=nuxeo --delete -- http://$S3_BUCKET.s3.amazonaws.com?prefix=$S3_BUCKET_PREFIX
+# Get and Delete S3 bucket prefix content
+/usr/bin/s3curl.pl --id nuxeo -- https://$S3_BUCKET.s3.amazonaws.com | grep -E -o '$S3_BUCKET_PREFIX[^<]+' | while read line; do
+    /usr/bin/s3curl.pl --id nuxeo --delete -- http://$S3_BUCKET.s3.amazonaws.com/"$line"
+done
