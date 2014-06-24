d209bf7db814c56287222dfea7258ddd97a8f977
Arnaud Kervern <akervern@nuxeo.com>
2014-06-24 15:47:18 +0200

    NXIO-202: Restart job instead of start while initialize node

--- systemd/node-init/start.sh
+++ systemd/node-init/start.sh
@@ -4,14 +4,14 @@ sleep 120
 
 # gogeta
 echo "Starting gogeta"
-/usr/bin/systemctl start gogeta.service
+/usr/bin/systemctl restart gogeta.service
 echo "Started gogeta"
 
 # ambs
 echo "Starting postgres amb"
-/usr/bin/systemctl start postgres-amb.service
+/usr/bin/systemctl restart postgres-amb.service
 echo "Started postgres amb"
 
 echo "Starting s3 amb"
-/usr/bin/systemctl start s3-amb.service
+/usr/bin/systemctl restart s3-amb.service
 echo "Started s3 amb"
