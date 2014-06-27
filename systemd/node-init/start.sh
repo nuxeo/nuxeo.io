c5904026b1ded35b481c73140b00772f0814f4bc
Arnaud Kervern <akervern@nuxeo.com>
2014-06-27 14:36:31 +0200

    NXIO-221: Ensure to run node-init as root

--- systemd/node-init/start.sh
+++ systemd/node-init/start.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-sleep 120
+sleep 60
 
 # gogeta
 echo "Starting gogeta"
-/usr/bin/systemctl restart gogeta.service
+sudo /usr/bin/systemctl restart gogeta.service
 echo "Started gogeta"
 
 # ambs
 echo "Starting postgres amb"
-/usr/bin/systemctl restart postgres-amb.service
+sudo /usr/bin/systemctl restart postgres-amb.service
 echo "Started postgres amb"
 
 echo "Starting s3 amb"
-/usr/bin/systemctl restart s3-amb.service
+sudo /usr/bin/systemctl restart s3-amb.service
 echo "Started s3 amb"
