5155f757c284c1cc741609bf2d8ffc536fb1704c
Arnaud Kervern <akervern@nuxeo.com>
2014-06-30 15:22:27 +0200

    NXIO-202: Remove useless param and unix rights

--- docker/io-container/start.sh
+++ docker/io-container/start.sh
@@ -30,7 +30,6 @@ perl -p -i -e "s/^#?nuxeo.db.password=.*$/nuxeo.db.password=$PG_PWD/g" $NUXEO_CO
 # S3 conf
 echo "nuxeo.core.binarymanager=org.nuxeo.ecm.core.storage.sql.S3BinaryManager" >> $NUXEO_CONF
 echo "nuxeo.s3storage.bucket=$S3_BUCKET" >> $NUXEO_CONF
-echo "nuxeo.s3storage.bucket.prefix=$S3_BUCKET_PREFIX" >> $NUXEO_CONF
 echo "nuxeo.s3storage.awsid=$S3_AWSID" >> $NUXEO_CONF
 echo "nuxeo.s3storage.awssecret=$S3_AWSSECRET" >> $NUXEO_CONF
 if [ $S3_AWSID = "nuxeo" ] ; then
