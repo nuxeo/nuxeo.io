94ce8698cc9da71599d42c24e9c991db12f5ff81
Thomas Roger <troger@nuxeo.com>
2014-07-18 14:52:07 +0200

    NXIO-242: use passivator and gogeta docker images version 0.1.0

--- fleet/passivator/start.sh
+++ fleet/passivator/start.sh
@@ -2,6 +2,6 @@
 
 /opt/data/tools/docker-clean.sh ${PASSIVATOR_NAME} &> /dev/null
 
-/usr/bin/docker pull arken/passivator
+/usr/bin/docker pull arken/passivator:0.1.0
 /usr/bin/docker run --rm --name $PASSIVATOR_NAME arken/passivator
 echo "Container $PASSIVATOR_NAME is running."
--- systemd/gogeta/start.sh
+++ systemd/gogeta/start.sh
@@ -2,6 +2,6 @@
 
 /opt/data/tools/docker-clean.sh ${GOGETA_NAME} &> /dev/null
 
-/usr/bin/docker pull arken/gogeta
+/usr/bin/docker pull arken/gogeta:0.1.0
 /usr/bin/docker run --rm --name $GOGETA_NAME -p 7777:7777 -v /opt/data/gogeta-templates:/gogeta-templates arken/gogeta -templateDir /gogeta-templates
 echo "Container $GOGETA_NAME is running."
