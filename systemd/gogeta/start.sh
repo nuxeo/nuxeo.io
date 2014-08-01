312e78bf73b7d3a336928a56597ff68228c31a14
Damien Metzler <dmetzler@nuxeo.com>
2014-08-02 00:40:55 +0200

    NXIO-287 use new param of gogeta to include nuxeo virtual host header

--- systemd/gogeta/start.sh
+++ systemd/gogeta/start.sh
@@ -3,5 +3,5 @@
 /opt/data/tools/docker-clean.sh ${GOGETA_NAME} &> /dev/null
 
 /usr/bin/docker pull arken/gogeta
-/usr/bin/docker run --rm --name $GOGETA_NAME -p 7777:7777 -v /opt/data/gogeta-templates:/gogeta-templates arken/gogeta -templateDir /gogeta-templates -forceFwSsl=true
+/usr/bin/docker run --rm --name $GOGETA_NAME -p 7777:7777 -v /opt/data/gogeta-templates:/gogeta-templates arken/gogeta -templateDir /gogeta-templates -forceFwSsl=true -UrlHeaderParam nuxeo-virtual-host
 echo "Container $GOGETA_NAME is running."
