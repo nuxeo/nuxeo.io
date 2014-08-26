d0483c793981f44ab4e7a24354ecf60fe8669bda
Thomas Roger <troger@nuxeo.com>
2014-08-26 15:12:05 +0200

    NXIO-305: release 0.2.0; use gogeta 0.2.1

--- systemd/gogeta/start.sh
+++ systemd/gogeta/start.sh
@@ -2,6 +2,6 @@
 
 /opt/data/tools/docker-clean.sh ${GOGETA_NAME} &> /dev/null
 
-/usr/bin/docker pull arken/gogeta
+/usr/bin/docker pull arken/gogeta:0.2.1
 /usr/bin/docker run --rm --name $GOGETA_NAME -p 7777:7777 -v /opt/data/gogeta-templates:/gogeta-templates arken/gogeta -templateDir /gogeta-templates -forceFwSsl=true -UrlHeaderParam nuxeo-virtual-host
 echo "Container $GOGETA_NAME is running."
