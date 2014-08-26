d4b3204451508ab6b633a66518f57ed87e17322c
Thomas Roger <troger@nuxeo.com>
2014-08-26 17:16:00 +0200

    NXIO-303: remove patched jar

--- docker/manager/nuxeo-install.sh
+++ docker/manager/nuxeo-install.sh
@@ -12,10 +12,6 @@ mv /tmp/nuxeo-distribution/$distdir/* $NUXEO_HOME
 rm -rf /tmp/nuxeo-distribution*
 chmod +x $NUXEO_HOME/bin/nuxeoctl
 
-# Patch nuxeo-platform-oauth with a newer version. TO BE REMOVED WHEN IN 5.9.5
-OAUTH_JAR=$(/bin/ls $NUXEO_HOME/nxserver/bundles/nuxeo-platform-oauth* | head -n 1)
-wget -q "https://maven-eu.nuxeo.org/nexus/service/local/repositories/public-snapshots/content/org/nuxeo/ecm/platform/nuxeo-platform-oauth/5.9.5-SNAPSHOT/nuxeo-platform-oauth-5.9.5-20140806.021345-36.jar" -O $OAUTH_JAR
-
 mkdir -p /var/lib/nuxeo
 mkdir -p /var/log/nuxeo
 mkdir -p /var/run/nuxeo
