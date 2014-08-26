0e4cbff95c548c2a1f690e8b4e9eb3e2d5b2bc5e
Arnaud Kervern <akervern@nuxeo.com>
2014-08-26 15:28:12 +0200

    NXIO-305: Extract, as possible, docker images versions from scripts

--- fleet/passivator/start.sh
+++ fleet/passivator/start.sh
@@ -2,6 +2,6 @@
 
 /opt/data/tools/docker-clean.sh ${PASSIVATOR_NAME} &> /dev/null
 
-/usr/bin/docker pull arken/passivator:0.1.0
-/usr/bin/docker run --rm --name $PASSIVATOR_NAME arken/passivator:0.1.0
+/usr/bin/docker pull arken/passivator:${PASSIVATOR_VERSION}
+/usr/bin/docker run --rm --name $PASSIVATOR_NAME arken/passivator:${PASSIVATOR_VERSION}
 echo "Container $PASSIVATOR_NAME is running."
