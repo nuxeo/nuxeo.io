71a6aa22600490aa81d6d227a26646f0eb0215b6
Arnaud Kervern <akervern@nuxeo.com>
2014-09-01 17:41:45 +0200

    NXIO-308: Add patched nuxeo-platform-web-common jar to container

--- docker/iocontainer/nuxeo-install.sh
+++ docker/iocontainer/nuxeo-install.sh
@@ -12,6 +12,11 @@ mv /tmp/nuxeo-distribution/$distdir/* $NUXEO_HOME
 rm -rf /tmp/nuxeo-distribution*
 chmod +x $NUXEO_HOME/bin/nuxeoctl
 
+# Patch nuxeo-platform-web-common with a newer version. TO BE REMOVED ASAP
+rm -fv $NUXEO_HOME/nxserver/bundles/nuxeo-platform-web-common-*
+wget -q "https://maven-us.nuxeo.org/nexus/service/local/repositories/public-releases/content/org/nuxeo/ecm/platform/nuxeo-platform-web-common/5.9.5.1/nuxeo-platform-web-common-5.9.5.1.jar" -P $NUXEO_HOME/nxserver/bundles
+## end patch
+
 mkdir -p /var/lib/nuxeo
 mkdir -p /var/lib/nuxeo/data
 mkdir -p /var/log/nuxeo
