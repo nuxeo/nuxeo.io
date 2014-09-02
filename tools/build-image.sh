1a0942520d9a1fff788dfe8f0a12a1d19368046e
Arnaud Kervern <akervern@nuxeo.com>
2014-09-02 15:12:41 +0200

    NXIO-308: Add options to build image script to do not use cache

--- tools/build-image.sh
+++ tools/build-image.sh
@@ -1,14 +1,46 @@
 #!/bin/bash -
 
+# usage
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-hc] DOCKER_IMAGE_NAME
+Build docker image named with the same name as the directory where it is. Also
+push it to the local registry.
+
+    -h          display this help and exit.
+    -c          use --no-cache param while building image
+EOF
+}
+
+# script variables
+no_cache=
+
+OPTIND=1 # Reset is necessary if getopts was used previously in the script.  It is a good idea to make this local in a function.
+while getopts "hc" opt; do
+    case "$opt" in
+        h)
+            show_help
+            exit 0
+            ;;
+        c)  no_cache="--no-cache"
+            ;;
+        '?')
+            show_help >&2
+            exit 1
+            ;;
+    esac
+done
+shift "$((OPTIND-1))" # Shift off the options and optional --.
+
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 DOCKER_IMAGE_NAME"
+  show_help >&2
   exit 1
 fi
 
 REGISTRY=`etcdctl get /services/docker-registry/1/location | sed -e 's/{"host":"\([^"]*\)","port":\([^"]*\)}/\1\:\2/'`
 
 # Build image
-/usr/bin/docker build -t nuxeo/$1 /opt/data/docker/$1/
+/usr/bin/docker build $no_cache -t nuxeo/$1 /opt/data/docker/$1/
 
 # Tag and push image on registry
 /usr/bin/docker tag nuxeo/$1 $REGISTRY/nuxeo/$1
