f884242aa6727d7d8e2e285f557301650fa94cde
Arnaud Kervern <akervern@nuxeo.com>
2014-09-10 15:36:53 +0200

    NXIO-314: Add a ttl to the container starting state

--- fleet/io-container/start-pre.sh
+++ fleet/io-container/start-pre.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/etcdctl set /services/$ENV_TECH_ID/1/status/current starting
+/usr/bin/etcdctl set --ttl $((7 * 60)) /services/$ENV_TECH_ID/1/status/current starting
 exec /opt/data/tools/initialize-bucket.sh
