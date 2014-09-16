91595fa597d9cbe13f6f2388d65ecfb3f36f3fda
Damien Metzler <dmetzler@nuxeo.com>
2014-09-16 14:24:44 +0200

    NXIO-306 don't build manager image locally

--- fleet/manager/start-pre.sh
+++ fleet/manager/start-pre.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# copy the coreos fleetctl binary for the manager
-cp /usr/bin/fleetctl /opt/data/docker/manager/fleetctl
-/usr/bin/docker build -t nuxeo/manager /opt/data/docker/manager/
-
 ENV_TECH_ID=manager exec /opt/data/tools/initialize-bucket.sh
