33a7f11691590f5bd58c4d9cc383689634689d06
Damien Metzler <dmetzler@nuxeo.com>
2014-09-18 15:02:18 +0200

    NXIO-319 init last access time in start-pre for a container

--- fleet/io-container/start-pre.sh
+++ fleet/io-container/start-pre.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 /usr/bin/etcdctl set --ttl $((7 * 60)) /services/$ENV_TECH_ID/1/status/current starting
+/usr/bin/etcdctl set /services/$ENV_TECH_ID/1/lastAccess "`/usr/bin/date +"%Y-%m-%d %H:%M:%S"`"
 /usr/bin/docker pull quay.io/nuxeoio/iocontainer
 
 exec /opt/data/tools/initialize-bucket.sh
