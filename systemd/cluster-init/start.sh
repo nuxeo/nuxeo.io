e82355c13afcc2659c02860a61a31011312e688b
Damien Metzler <dmetzler@nuxeo.com>
2014-10-21 17:11:58 +0200

    NXIO-326 Start a 3 nodes ES cluster

--- systemd/cluster-init/start.sh
+++ systemd/cluster-init/start.sh
@@ -8,16 +8,14 @@ fi
 /usr/bin/etcdctl set /_arken.io/initialized true
 
 fleetctl start /opt/data/fleet/passivator/passivator.service
-
-#fleetctl start /opt/data/fleet/docker-registry/docker-registry.service
-#/opt/data/tools/wait-fleet-unit.sh docker-registry.service
-
-# start fill-registry script
-#/opt/data/tools/fill-registry.sh
-
 fleetctl start /opt/data/fleet/manager/manager.service
-
-/opt/data/tools/wait-fleet-unit.sh build-etcdump.service
 fleetctl start /opt/data/fleet/etcdump/etcdump.service
+
+# Start an elasticsearch cluster
+fleetctl submit /opt/data/fleet/elasticsearch-discovery@{1..3}.service
+fleetctl start /opt/data/fleet/elasticsearch@1.service
+sleep 10
+fleetctl start /opt/data/fleet/elasticsearch@{2..3}.service
+
 
 /opt/data/tools/wait-fleet-unit.sh manager.service
