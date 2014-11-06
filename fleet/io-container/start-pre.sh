5433e0e587bb56286c01430d34d2395845775279
Arnaud Kervern <akervern@nuxeo.com>
2014-11-06 15:30:20 +0100

    NXIO-333: Specify iocontainer tag to start

--- fleet/io-container/start-pre.sh
+++ fleet/io-container/start-pre.sh
@@ -2,6 +2,5 @@
 
 /usr/bin/etcdctl set --ttl $((7 * 60)) /services/$ENV_TECH_ID/1/status/current starting
 /usr/bin/etcdctl set /services/$ENV_TECH_ID/1/lastAccess "`/usr/bin/date +"%Y-%m-%d %H:%M:%S"`"
-/usr/bin/docker pull quay.io/nuxeoio/iocontainer
 
 exec /opt/data/tools/initialize-bucket.sh
