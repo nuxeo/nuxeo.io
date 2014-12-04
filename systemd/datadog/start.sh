2e568eb03d75d43f3f8173ea8f18f07f143b20d6
Arnaud Kervern <akervern@nuxeo.com>
2014-12-04 15:28:22 +0100

    NXIO-347: Add HEAP memory to ES container

--- systemd/datadog/start.sh
+++ systemd/datadog/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+/opt/data/tools/docker-clean.sh dd-agent &> /dev/null
+
 etcdctl get /config/datadog/key 2>&1 > /dev/null
 if [ $? -eq 0 ]; then
-    /usr/bin/docker run -d --privileged --name dd-agent -h `hostname` -v /var/run/docker.sock:/var/run/docker.sock -v /proc/mounts:/host/proc/mounts:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e API_KEY=`etcdctl get /config/datadog/key`  datadog/docker-dd-agent
-    echo "Started Datadog agent"
+    /usr/bin/docker run --rm --privileged --name dd-agent -h `hostname` -v /var/run/docker.sock:/var/run/docker.sock -v /proc/mounts:/host/proc/mounts:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e API_KEY=`etcdctl get /config/datadog/key`  datadog/docker-dd-agent
 else
     echo "API key not configured (/config/datadog/key), not starting dd-agent"
     exit 1
