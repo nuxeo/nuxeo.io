68b0405c6c2065132b3ec520974f857704a23dd7
Arnaud Kervern <akervern@nuxeo.com>
2014-12-24 15:44:29 +0100

    NXIO-358: Fix wrong docker run cmd

--- systemd/journald-wrapper/start.sh
+++ systemd/journald-wrapper/start.sh
@@ -6,5 +6,8 @@ AWS_ACCESS_ID=`/usr/bin/etcdctl get /_arken.io/config/aws/id`
 AWS_ACCESS_SECRET=`/usr/bin/etcdctl get /_arken.io/config/aws/secret`
 AWS_REGION=`/usr/bin/etcdctl get /_arken.io/config/s3/region`
 PREFIX=`/usr/bin/etcdctl get /_arken.io/key`
+CURSOR_PATH=/data/journald
 
-exec docker run --name journald-wrapper -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_ID -e AWS_SECRET_ACCESS_KEY=$AWS_ACCESS_SECRET -e AWS_REGION=$AWS_REGION -e PREFIX=$PREFIX --rm quay.io/nuxeoio/journald-wrapper
+mkdir -p /data/journald
+
+exec docker run --name journald-wrapper -v /data/journald:/data/journald -e CURSOR_PATH=$CURSOR_PATH -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_ID -e AWS_SECRET_ACCESS_KEY=$AWS_ACCESS_SECRET -e AWS_REGION=$AWS_REGION -e PREFIX=$PREFIX --rm quay.io/nuxeoio/journald-wrapper
