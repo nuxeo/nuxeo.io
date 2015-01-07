e04c96589a792a846b9ecfb4bfa4637f8e68339f
Arnaud Kervern <akervern@nuxeo.com>
2015-01-07 15:16:53 +0100

    NXIO-345: Fix arkenwatch start script

--- fleet/arkenwatch/start.sh
+++ fleet/arkenwatch/start.sh
@@ -2,10 +2,4 @@
 
 /opt/data/tools/docker-clean.sh arken-watch &> /dev/null
 
-etcdctl get /config/datadog/key 2>&1 > /dev/null
-if [ $? -eq 0 ]; then
-    /usr/bin/docker run --rm --name arken-watch -h `hostname` -e DD_API_KEY=`etcdctl get /config/datadog/key` quay.io/nuxeoio/arkenctl watch
-else
-    echo "API key not configured (/config/datadog/key), not starting dd-agent"
-    exit 1
-fi
+exec /usr/bin/docker run --rm --name arken-watch -h `hostname` -e DD_API_KEY=`etcdctl get /config/datadog/key` quay.io/nuxeoio/arkenctl watch
