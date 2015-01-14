99443c83dd2f424450af15c7d29fbb36c812f15e
Arnaud Kervern <akervern@nuxeo.com>
2015-01-14 15:29:12 +0100

    NXIO-379: Use fleet template to manager ES and fix ES heap param

--- tools/restart-es.sh
+++ tools/restart-es.sh
@@ -1,14 +1,20 @@
 #!/bin/sh -
 echo "Destroying old units"
 for i in 1 2 3; do
-  fleetctl destroy elasticsearch-${i}.service
-  fleetctl destroy elastic-discovery-${i}.service
+  fleetctl destroy elasticsearch@${i}.service
+  fleetctl destroy elasticdiscovery@${i}.service
 done
+
+fleetctl destroy /opt/data/fleet/elasticsearch/elasticsearch@.service
+fleetctl submit /opt/data/fleet/elasticsearch/elasticsearch@.service
+
+fleetctl destroy /opt/data/fleet/elasticsearch/elasticdiscovery@.service
+fleetctl submit /opt/data/fleet/elasticsearch/elasticdiscovery@.service
 
 echo "Starting new units"
 for i in 1 2 3; do
-  fleetctl start /opt/data/fleet/elasticsearch/elasticsearch-${i}.service
+  fleetctl start /opt/data/fleet/elasticsearch/elasticsearch@${i}.service
   sleep 15
-  fleetctl start /opt/data/fleet/elasticsearch/elastic-discovery-${i}.service
+  fleetctl start /opt/data/fleet/elasticsearch/elasticdiscovery@${i}.service
   sleep 10
 done;
