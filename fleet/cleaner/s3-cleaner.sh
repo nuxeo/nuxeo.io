c1e1e84844dc150e6a04743b0c8d7ee8e56ccf77
Damien Metzler <dmetzler@nuxeo.com>
2015-01-19 17:37:44 +0100

    NXIO-375 don't specify region for S3 API

--- fleet/cleaner/s3-cleaner.sh
+++ fleet/cleaner/s3-cleaner.sh
@@ -5,7 +5,7 @@ S3_BUCKET=`etcdctl get /services/${SERVICE_ID}/config/s3/bucket`
 S3_REGION=`/usr/bin/etcdctl get /_arken.io/config/s3/region`
 USERNAME=`etcdctl get /services/${SERVICE_ID}/config/s3/username`
 
-/opt/data/tools/awscli s3 rm --region ${S3_REGION} --recursive s3://${S3_BUCKET}/${SERVICE_ID}
+/opt/data/tools/awscli s3 rm --recursive s3://${S3_BUCKET}/${SERVICE_ID}
 /opt/data/tools/awscli iam delete-access-key --access-key-id ${S3_ACCESS_KEY} --user-name ${USERNAME}
 /opt/data/tools/awscli iam delete-user-policy --policy-name ${USERNAME} --user-name ${USERNAME}
 /opt/data/tools/awscli iam delete-user --user-name ${USERNAME}
