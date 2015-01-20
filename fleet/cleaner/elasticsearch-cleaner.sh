6b83c892110a0ccccdf07abe037b471ff4ec7d5c
Arnaud Kervern <akervern@nuxeo.com>
2015-01-20 17:25:44 +0100

    NXIO-375: Lookup an ES host from etcd instead of assuming localhost

--- fleet/cleaner/elasticsearch-cleaner.sh
+++ fleet/cleaner/elasticsearch-cleaner.sh
@@ -1,4 +1,15 @@
 #!/bin/sh -
 
+HOST=`etcdctl ls --recursive /services/elasticsearch \
+  | grep transport \
+  | head -n 1 \
+  | xargs etcdctl get \
+  | sed -e 's/{"host":"\([^"]*\)","port":\([^"]*\)}/\1\:9200/'`
+
 # Delete ElasticSearch index
-curl -XDELETE "http://localhost:9200/${SERVICE_ID}"
+if [ $? -eq 0 ] && [ -n "${HOST}" ]; then
+  curl -XDELETE "http://${HOST}/${SERVICE_ID}"
+else
+  echo "Unable to correctly clean ES"
+  exit 1
+fi
