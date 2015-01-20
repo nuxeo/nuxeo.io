a1310bf1851682a5421b4fd5ace03a76422ec1a9
Arnaud Kervern <akervern@nuxeo.com>
2015-01-20 10:22:53 +0100

    NXIO-375: Secure etcd cleaner

--- fleet/cleaner/etcd-cleaner.sh
+++ fleet/cleaner/etcd-cleaner.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -
 
-DOMAIN=`etcdctl get /services/${SERVICE_ID}/1/domain`
-
+if [ -z "${SERVICE_ID}" ]; then
+  exit 1
+fi
 etcdctl rm --recursive /services/${SERVICE_ID}
-etcdctl rm --recursive /domains/${DOMAIN}
+
+DOMAIN=`etcdctl get /services/${SERVICE_ID}/1/domain`
+if [ $? -eq 0 ]; then
+  etcdctl rm --recursive /domains/${DOMAIN}
+fi
