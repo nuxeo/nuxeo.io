a10df5f15aa3d54ba81e3fd8be2de91ec4a6ff02
Damien Metzler <dmetzler@nuxeo.com>
2015-02-05 12:23:13 +0100

    NXIO-394 wait for service to stop before cleaning

--- fleet/cleaner/start.sh
+++ fleet/cleaner/start.sh
@@ -5,6 +5,10 @@ UNITNAME=nxio@`echo $SERVICE_ID | cut -d_ -f2`.service
 
 /usr/bin/fleetctl destroy $UNITNAME
 
+while [ `etcdctl get /services/$SERVICE_ID/1/status/current` != "stopped" ]; do
+	echo "Waiting for service to stop"; sleep 5;
+done
+
 echo "Deleting entries for $SERVICE_ID"
 for SERVICE in "$@"
 do
