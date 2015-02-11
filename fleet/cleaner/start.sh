6f1c90ca718db17ca31670a92939b2faaa762cdc
Damien Metzler <dmetzler@nuxeo.com>
2015-02-11 07:12:44 +0100

    NXIO-394 add a timeout when waiting for an env to be stopped, and verify tools are present

--- fleet/cleaner/start.sh
+++ fleet/cleaner/start.sh
@@ -3,20 +3,38 @@
 BASE_PATH=`dirname $0`
 UNITNAME=nxio@`echo $SERVICE_ID | cut -d_ -f2`.service
 
-/usr/bin/fleetctl destroy $UNITNAME
+if [ ! -f /usr/bin/fleetctl ]; then
+    echo "Error: fleetctl not available"
+    exit 1
+fi
 
+if [ ! -f /usr/bin/etcdctl ]; then
+    echo "Error: etcdctl not available"
+    exit 1
+fi
+
+
+/usr/bin/fleetctl destroy $UNITNAME
+i=0
 while [ `etcdctl get /services/$SERVICE_ID/1/status/current` != "stopped" ]; do
-	echo "Waiting for service to stop"; sleep 5;
+    echo "Waiting for service to stop"
+    sleep 5
+    i=$((i+1))
+	if [ $i -eq 20 ]; then
+        echo "Timeout waiting for service to stop, exiting in error"
+		exit 1;
+	fi
 done
 
 echo "Deleting entries for $SERVICE_ID"
 for SERVICE in "$@"
 do
     echo "Cleaning $SERVICE:"
+
     CLEANER="$BASE_PATH/$SERVICE-cleaner.sh"
     if [ -f $CLEANER ]; then
         $CLEANER
     else
-        echo "Warning: Unknown service $SERVICE."
+        echo "Warning: No cleaner for service $SERVICE."
     fi
 done
