108a8d1e1397877e6d14466e607363b91fefb13c
Damien Metzler <dmetzler@nuxeo.com>
2015-02-11 10:36:38 +0100

    NXIO-394 use --endpoint-url if needed and verify presence of every variable or executable

--- fleet/cleaner/s3-cleaner.sh
+++ fleet/cleaner/s3-cleaner.sh
@@ -1,11 +1,42 @@
 #!/bin/sh -
 
-S3_ACCESS_KEY=`etcdctl get /services/${SERVICE_ID}/config/s3/awsid`
-S3_BUCKET=`etcdctl get /services/${SERVICE_ID}/config/s3/bucket`
-S3_REGION=`/usr/bin/etcdctl get /_arken.io/config/s3/region`
-USERNAME=`etcdctl get /services/${SERVICE_ID}/config/s3/username`
+if [ -z "$var" ]; then
+  echo "Service ID is empty, exiting $0"
+  exit 1
+fi
 
-/opt/data/tools/awscli s3 rm --recursive s3://${S3_BUCKET}/${SERVICE_ID}
-/opt/data/tools/awscli iam delete-access-key --access-key-id ${S3_ACCESS_KEY} --user-name ${USERNAME}
-/opt/data/tools/awscli iam delete-user-policy --policy-name ${USERNAME} --user-name ${USERNAME}
-/opt/data/tools/awscli iam delete-user --user-name ${USERNAME}
+ETCDCTL=/usr/bin/etcdctl
+if [ ! -f $ETCDCTL ]; then
+  echo "Missing $ETCDCTL, exiting $0"
+  exit 1
+fi
+
+AWSCLI=/opt/data/tools/awscli
+if [ ! -f $AWSCLI ]; then
+  echo "Missing $AWSCLI, exiting $0"
+  exit 1
+fi
+
+
+S3_ACCESS_KEY=`$ETCDCTL get /services/${SERVICE_ID}/config/s3/awsid`
+[ -z $S3_ACCESS_KEY ] && echo "No AWS access key, exiting $0" && exit 1
+S3_BUCKET=`$ETCDCTL get /services/${SERVICE_ID}/config/s3/bucket`
+[ -z $S3_ACCESS_KEY ] && echo "No AWS bucket configured for $SERVICE_ID, exiting $0" && exit 1
+S3_REGION=`$ETCDCTL get /_arken.io/config/s3/region`
+[ -z $S3_ACCESS_KEY ] && echo "No AWS access region configured, exiting $0" && exit 1
+USERNAME=`$ETCDCTL get /services/${SERVICE_ID}/config/s3/username`
+[ -z $S3_ACCESS_KEY ] && echo "No AWS username configured for $SERVICE_ID, exiting $0" && exit 1
+
+
+
+
+if [ $S3_REGION != "us-east-1" ]; then
+  $AWSCLI s3 rm --recursive --endpoint-url https://s3-$REGION.amazonaws.com/ s3://${S3_BUCKET}/${SERVICE_ID}
+else
+  $AWSCLI s3 rm --recursive s3://${S3_BUCKET}/${SERVICE_ID}
+fi
+
+$AWSCLI s3 rm --recursive s3://${S3_BUCKET}/${SERVICE_ID}
+$AWSCLI iam delete-access-key --access-key-id ${S3_ACCESS_KEY} --user-name ${USERNAME}
+$AWSCLI iam delete-user-policy --policy-name ${USERNAME} --user-name ${USERNAME}
+$AWSCLI iam delete-user --user-name ${USERNAME}
--- tools/initialize-bucket.sh
+++ tools/initialize-bucket.sh
@@ -1,39 +1,62 @@
 #!/bin/sh
 
-PREFIX=`etcdctl get /_arken.io/key`
-USERNAME=${PREFIX}-${ENV_TECH_ID}
-BUCKET=`etcdctl get /_arken.io/config/s3/bucket`
-RAND=`openssl rand -hex 5`
-DIRECTORY=${ENV_TECH_ID}
-
-/opt/data/tools/awscli &> /dev/null
-if [ ! $? -eq 2 ]; then
-  echo "AWSCLI is missing."
+ETCDCTL=/usr/bin/etcdctl
+if [ ! -f $ETCDCTL ]; then
+  echo "Missing $ETCDCTL, exiting $0"
   exit 1
 fi
 
-# Creating S3 user if needed
-/opt/data/tools/awscli iam get-user --user-name $USERNAME &> /dev/null
-if [ ! $? -eq 0 ]; then
-  /opt/data/tools/awscli iam create-user --user-name $USERNAME --path /container/${PREFIX}/
+AWSCLI=/opt/data/tools/awscli
+if [ ! -f $AWSCLI ]; then
+  echo "Missing $AWSCLI, exiting $0"
+  exit 1
 fi
-etcdctl set /services/${ENV_TECH_ID}/config/s3/bucket ${BUCKET}
-etcdctl set /services/${ENV_TECH_ID}/config/s3/username ${USERNAME}
 
-COUNT=`/opt/data/tools/awscli iam list-user-policies --user-name $USERNAME | wc -l`
+$AWSCLI &> /dev/null
+if [ ! $? -eq 2 ]; then
+  echo "AWSCLI is missing, exiting $0"
+  exit 1
+fi
+
+[ -z $ENV_TECH_ID ] && echo "No environment tech id given, exiting $0" && exit 1
+PREFIX=`$ETCDCTL get /_arken.io/key`
+[ -z $PREFIX ] && echo "No arken prefix configured, exiting $0" && exit 1
+USERNAME=${PREFIX}-${ENV_TECH_ID}
+BUCKET=`$ETCDCTL get /_arken.io/config/s3/bucket`
+[ -z $BUCKET ] && echo "No bucket configured for this cluster, exiting $0" && exit 1
+REGION=`$ETCDCTL get /_arken.io/config/s3/region`
+[ -z $REGIONS ] && echo "No region configured for this cluster, exiting $0" && exit 1
+RAND=`openssl rand -hex 5`
+DIRECTORY=${ENV_TECH_ID}
+
+
+# Creating S3 user if needed
+$AWSCLI iam get-user --user-name $USERNAME &> /dev/null
+if [ ! $? -eq 0 ]; then
+  echo "Creating user $USERNAME for $ENV_TECH_ID"
+  $AWSCLI iam create-user --user-name $USERNAME --path /container/${PREFIX}/
+fi
+$ETCDCTL set /services/${ENV_TECH_ID}/config/s3/bucket ${BUCKET}
+$ETCDCTL set /services/${ENV_TECH_ID}/config/s3/username ${USERNAME}
+
+COUNT=`$AWSCLI iam list-user-policies --user-name $USERNAME | wc -l`
 if [ $COUNT -eq 0 ]; then
   # UserPolicy to allow user only on his own directory
   USER_POLICY="{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"${RAND}1\",\"Effect\":\"Allow\",\"Action\":[\"s3:*\"],\"Resource\":[\"arn:aws:s3:::${BUCKET}\"]},{\"Sid\":\"${RAND}2\",\"Effect\":\"Allow\",\"Action\":[\"s3:*\"],\"Resource\":[\"arn:aws:s3:::${BUCKET}/${DIRECTORY}/*\"]},{\"Sid\":\"${RAND}3\",\"Effect\":\"Allow\",\"Action\":[\"s3:ListAllMyBuckets\"],\"Resource\":[\"arn:aws:s3:::${BUCKET}\"]}]}"
-  /opt/data/tools/awscli iam put-user-policy --user-name ${USERNAME} --policy-name ${USERNAME} --policy-document "${USER_POLICY}"
+  $AWSCLI iam put-user-policy --user-name ${USERNAME} --policy-name ${USERNAME} --policy-document "${USER_POLICY}"
 fi
 
-`etcdctl get /services/${ENV_TECH_ID}/config/s3/awsid &> /dev/null`
+`$ETCDCTL get /services/${ENV_TECH_ID}/config/s3/awsid &> /dev/null`
 if [ ! $? -eq 0 ]; then
-  KEYS_RES=`/opt/data/tools/awscli iam create-access-key --user-name ${USERNAME}`
+  KEYS_RES=`$AWSCLI iam create-access-key --user-name ${USERNAME}`
 
-  etcdctl set /services/${ENV_TECH_ID}/config/s3/awsid `echo ${KEYS_RES} | awk '{print $2}'`
-  etcdctl set /services/${ENV_TECH_ID}/config/s3/awssecret `echo ${KEYS_RES} | awk '{print $4}'`
+  $ETCDCTL set /services/${ENV_TECH_ID}/config/s3/awsid `echo ${KEYS_RES} | awk '{print $2}'`
+  $ETCDCTL set /services/${ENV_TECH_ID}/config/s3/awssecret `echo ${KEYS_RES} | awk '{print $4}'`
 fi
 
 # Check bucket/awscli up and running
-exec /opt/data/tools/awscli s3api head-bucket --bucket ${BUCKET}
+if [ $REGION != "us-east-1" ]; then
+  exec $AWSCLI s3api head-bucket --endpoint-url https://s3-$REGION.amazonaws.com/ --bucket ${BUCKET}
+else
+  exec $AWSCLI s3api head-bucket --bucket ${BUCKET}
+fi
